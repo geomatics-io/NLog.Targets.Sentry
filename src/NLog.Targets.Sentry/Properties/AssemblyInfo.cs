1197240ba1a3d32b1645818abab94ff759aa610b
Christian Junk <christian.junk@woodplc.com>
2020-01-23 01:35:46 +0100

    Moved projects under src directory. Switched back to .NET 4.6.1.

--- src/NLog.Targets.Sentry/Properties/AssemblyInfo.cs
+++ src/NLog.Targets.Sentry/Properties/AssemblyInfo.cs
@@ -1,15 +1,21 @@
-﻿using System.Reflection;
+﻿#region
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+
+#endregion
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NLog.Targets.Sentry.UnitTests")]
-[assembly: AssemblyDescription("Custom target for NLog enabling you to send logging messages to the Sentry logging service.")]
+[assembly: AssemblyTitle("NLog.Targets.Sentry")]
+[assembly:
+    AssemblyDescription("Custom target for NLog enabling you to send logging messages to the Sentry logging service.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bjarne Riis, Dillon Buchanan, Christian Junk")]
-[assembly: AssemblyProduct("NLog.Targets.Sentry.UnitTests")]
-[assembly: AssemblyCopyright("Copyright © 2015-2018 Bjarne Riis, Christian Junk")]
+[assembly: AssemblyProduct("NLog.Targets.Sentry")]
+[assembly: AssemblyCopyright("Copyright © 2016-2018 Bjarne Riis, Christian Junk")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,7 +25,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1c924fcc-b923-49a0-b6bd-c1cca48f36e7")]
+[assembly: Guid("e14fa010-29e4-48a5-9268-a26215202a72")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,3 +39,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0")]
 [assembly: AssemblyFileVersion("2.0.0")]
+[assembly: InternalsVisibleTo("NLog.Targets.Sentry.UnitTests")]
