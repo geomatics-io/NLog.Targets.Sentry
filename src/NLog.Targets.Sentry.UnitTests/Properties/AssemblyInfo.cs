3bd89b7666a879f7cc4d1718fa41cf9289bfb6cc
Christian Junk <christian.junk@woodplc.com>
2020-01-23 02:43:58 +0100

    Complete repackaging and preparation for AppVeyor.

--- src/NLog.Targets.Sentry.UnitTests/Properties/AssemblyInfo.cs
+++ src/NLog.Targets.Sentry.UnitTests/Properties/AssemblyInfo.cs
@@ -1,7 +1,8 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NLog.Targets.Sentry.UnitTests")]
@@ -9,27 +10,28 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bjarne Riis, Dillon Buchanan, Christian Junk")]
 [assembly: AssemblyProduct("NLog.Targets.Sentry.UnitTests")]
-[assembly: AssemblyCopyright("Copyright © 2015-2018 Bjarne Riis, Christian Junk")]
+[assembly: AssemblyCopyright("©Copyright 2015-2020 Bjarne Riis, Christian Junk")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1c924fcc-b923-49a0-b6bd-c1cca48f36e7")]
+[assembly: Guid("0371d166-5f24-4c1d-900e-c51d04e90d03")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
